4da59181c5f9b49dd9e638ce9c2a063b4757d57b
steadymingha <astromingha@gmail.com>
2023-12-06 08:34:17 +0900

    after fight

--- fight.py
+++ fight.py
@@ -13,9 +13,19 @@ import slimevolleygym
 from PPO import PPO
 from gym.wrappers.monitoring.video_recorder import VideoRecorder
 
-np.set_printoptions(threshold=20, precision=3, suppress=True, linewidth=200)
 
-RENDER_MODE = True
+MH = False
+SH = True
+Video = False
+YS = True
+
+Record = False
+video_name = 'Skynet/record/좌영상우승현.mp4'
+
+
+
+
+
 
 
 class PPOPolicy:
@@ -27,22 +37,14 @@ class PPOPolicy:
         return action
 
 if __name__=="__main__":
-
-
-    if RENDER_MODE:
-        import pyglet
-        from pyglet.window import key
-        from time import sleep
+    from pyglet.window import key
+    from time import sleep
 
     env = gym.make("SlimeVolley-v0")
     env.seed(np.random.randint(0, 10000))
 
-
-    if RENDER_MODE:
-        env.render()
-
-
-
+    env.render()
+    if Record: video_recorder = VideoRecorder(env, video_name)
 
 
     # ##################################################MH###########################
@@ -51,26 +53,29 @@ if __name__=="__main__":
     # checkpoint_path = '/home/user/RLstudy/slimevolleygym/PPO_preTrained/SlimeVolley-v0/PPO_SlimeVolley-v0_0_0.pth'
     ##################################################MH###########################
     # initialize a SH agent
-    from collections import namedtuple
-    from Skynet.seunghyun_slime_a2c_v2 import Policy
-    gamma = 0.99
-    SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
-    lr = 5e-4
-    sh_agent = Policy(12,6)
-    checkpoint_path = 'Skynet/volley_actor_v3_new.pth'
-    sh_agent.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
+    if SH:
+        from collections import namedtuple
+        from Skynet.seunghyun_slime_a2c_v2 import Policy
+        gamma = 0.99
+        SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
+        lr = 5e-4
+        sh_agent = Policy(12,6)
+        checkpoint_path = 'Skynet/volley_actor_v3_new.pth'
+        sh_agent.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
     #################################################MH###########################
-    # intialize a Video agent
-    from stable_baselines3 import PPO
+    # # intialize a Video agent
+    if Video:
+        from stable_baselines3 import PPO
+        video_agent = PPOPolicy('Skynet/video_model.zip')
 
-    video_agent = PPOPolicy('Skynet/video_model.zip')
     ##################################################MH###########################
     # initialize a YS agent
-    from Skynet.yongsun_final_script import ActorCritic
-    ys_agent = ActorCritic(12,128,6)
-    # checkpoint_path = 'Skynet/yongsun_agent_right.pth'
-    checkpoint_path = 'Skynet/yongsun_agent_left.pth'
-    ys_agent.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
+    if YS:
+        from Skynet.yongsun_final_script import ActorCritic
+        ys_agent = ActorCritic(12, 128, 6)
+        # checkpoint_path = 'Skynet/yongsun_agent_right.pth'
+        checkpoint_path = 'Skynet/yongsun_agent_left.pth'
+        ys_agent.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage))
 
     #################################################MH###########################
 
@@ -79,26 +84,29 @@ if __name__=="__main__":
     state_left = state_right.copy()
     total_reward = 0
     done = False
+
     while not done:
+        if SH:
+            action_right = sh_agent.select_action(state_right)
+            action_right = env.action_table[action_right]
 
-        action_right = sh_agent.select_action(state_right)
-        action_right = env.action_table[action_right]
+        if Video:
+            action_right = video_agent.select_action(state_right)
+            # action_right = video_agent.select_action(state_right)
 
+        if YS:
+            action_left = ys_agent.select_action(state_left)
+            # action_right = ys_agent.select_action(state_right)
 
-        # action_right = video_agent.select_action(state_right)
-
-
-        action_left = ys_agent.select_action(state_left)
-        # action_left = ys_agent.select_action(state_right)
-
-        state_right, reward, done, info = env.step(action_right, action_left)#, action_left)
+        state_right, reward, done, info = env.step(action_right, action_left)
         state_left = info['otherState']
 
         total_reward += reward
 
-        if RENDER_MODE:
-          env.render()
-          sleep(0.02)
+        if Record: video_recorder.capture_frame()
+        env.render()
+        # sleep(0.02)
 
     env.close()
+    if Record: video_recorder.close()
     print("cumulative score", total_reward)
